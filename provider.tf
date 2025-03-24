4f07f505714895afb5515c0dfe8d68b035da2aff
SefaliSabnam <sefali.sabnam26@gmail.com>
2025-03-24 14:39:08 +0530

    new

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-s3-jenkins"  # Your S3 bucket name
-    key            = "terraform.tfstate"        # State file path
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"           # DynamoDB table for state locking
+    bucket        = "my-terraform-s3-jenkins"  # S3 bucket for state file
+    key           = "terraform.tfstate"        # State file path
+    region        = "ap-south-1"
+    encrypt       = true
+    use_lockfile  = true  # Replaces 'dynamodb_table'
   }
 
   required_providers {
