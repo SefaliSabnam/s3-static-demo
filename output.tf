038e4a7e6b76f88c1652ffdda908bbf54f05f1e9
SefaliSabnam <sefali.sabnam26@gmail.com>
2025-03-24 21:37:21 +0530

    done3

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "website_url" {
-  value = "http://${aws_s3_bucket.website_bucket.bucket}.s3-website-${var.aws_region}.amazonaws.com"
+  value = "http://${aws_s3_bucket.static_website.bucket}.s3-website-${var.aws_region}.amazonaws.com"
 }
